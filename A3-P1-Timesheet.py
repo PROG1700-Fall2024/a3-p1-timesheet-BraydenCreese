106e9b79e9da952906403958b68d6295ce7992b8
BraydenCreese <kidson2017@gmail.com>
2024-11-21 21:38:26 -0800

    Done. 1 Day Late

--- A3-P1-Timesheet.py
+++ A3-P1-Timesheet.py
@@ -3,19 +3,56 @@
 #               each of five work days from the user, then displays different 
 #               information calculated about those entries as output. 
 
-#Student #:     
-#Student Name:  
+#Student #: Brayden Creese
+#Student Name: W0491583
+
+def timeSheet():
+
+    dailyHours = []
+
+    for i in range(5):
+        while True:
+            try:
+                hoursWorked = float(input(f"Enter hours worked on Day #{i + 1}: "))
+                if hoursWorked < 0:
+                    print("Invalid Input! Please enter a postive number for 'hours worked'.")
+                    continue
+                dailyHours.append(hoursWorked)
+                break
+            except ValueError:
+                print("Invalid Input! Please enter a valid number for 'hours worked'.")
+
+    print("-" * 60)
+
+
+    mostHours = max(dailyHours)
+    mostHourDay = []
+    for i in range(len(dailyHours)):
+        if dailyHours[i] == mostHours:
+            mostHourDay.append(f"Day {i+1}")
+
+    totalHours = sum(dailyHours)
+    averageHours = totalHours / len(dailyHours)
+
+    slackDays = []
+    for i in range(len(dailyHours)):
+        if dailyHours[i] < 7:
+            slackDays.append(f"Day {i+1}")
+
+
+    return mostHourDay, mostHours, totalHours, averageHours, slackDays
+
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    mostHourDay, mostHours, totalHours, averageHours, slackDays = timeSheet()
 
+    print(f"The most hours worked was on:\n {mostHourDay} when you worked {mostHours} hours.")
+    print("-" * 60)
+    print(f"The total number of hours worked was: {totalHours}")
+    print(f"The average number of hours worked on each day was: {averageHours}")
+    print("-" * 60)
+    print(f"Days you slacked off (i.e. worked less then 7 hours): {', '.join(slackDays) if slackDays else 'None'}") 
 
-
-
-
-
-
-    # YOUR CODE ENDS HERE
 
 main()
